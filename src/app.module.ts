e77d37318c3347ca73307db8c8cb3e0b7bac0b7b
Radialis <nursultanadilbekov5@gmail.com>
2026-03-12 14:17:21 +0600

    feat: connect PostgreSQL with TypeORM and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
